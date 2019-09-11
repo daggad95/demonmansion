41dceefd64ed596bcbcd3f9aa7586edcdb4d9980
David Garber <daggad95@gmail.com>
2019-09-10 21:31:43 -0700

    Added basic player scene

---
+++ scenes/player/Player.gd
@@ -0,0 +1,31 @@
+extends KinematicBody2D
+
+class_name Player
+
+var health    = 100
+var money     = 0
+var inventory = []
+var speed = 100
+var velocity = Vector2()
+
+func get_input():
+	velocity = Vector2()
+	if Input.is_action_pressed('player_right'):
+	    velocity.x += 1
+	if Input.is_action_pressed('player_left'):
+	    velocity.x -= 1
+	if Input.is_action_pressed('player_down'):
+	    velocity.y += 1
+	if Input.is_action_pressed('player_up'):
+	    velocity.y -= 1
+	velocity = velocity.normalized() * speed
+
+func get_money():
+	return money
+
+func get_inventory():
+	return inventory
+
+func _physics_process(delta):
+	get_input()
+	move_and_slide(velocity)
