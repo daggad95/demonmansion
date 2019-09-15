ba4be140e1ac215459c4ec07bd2e3f11e502d193
David Garber <daggad95@gmail.com>
2019-09-14 20:53:36 -0700

    made changes on master sorry

--- scenes/enemy/Enemy.gd
+++ scenes/enemy/Enemy.gd
@@ -1,14 +1,39 @@
-extends KinematicBody2D
+extends RigidBody2D
+class_name Enemy
 
-var speed
+var speed = 75
+var steer_rate = 200
 var map
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	speed = 75
 	map = get_parent().get_node("Map")
+	add_to_group('enemy')
 	
 func _physics_process(delta):
-	var target_dir = map.get_vector_to_target(position)
-	var target_vel = target_dir.normalized() * speed
-	move_and_slide(target_vel)
+	var target_dir
+	var target_vel
+	var near_enemy = false
+	var nearby_enemies = []
+	
+	
+	if len($Bubble.get_overlapping_bodies()) > 0:
+		for body in $Bubble.get_overlapping_bodies():
+			if body.is_in_group('enemy') and body != self:
+				near_enemy = true
+				nearby_enemies.append(body)
+				
+	if near_enemy:
+		target_dir = Vector2(0, 0)
+		for enemy in nearby_enemies:
+			print(position - enemy.position)
+			target_dir += (position - enemy.position)
+			target_vel = target_dir.normalized() * speed / 4
+	else:
+		target_dir = map.get_vector_to_target(position)
+		target_vel = target_dir.normalized() * speed
+		
+	rotation = 0
+	applied_force = Vector2(0, 0)
+	var f = (target_vel - linear_velocity).normalized() * steer_rate
+	add_central_force(f)
