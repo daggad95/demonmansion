f5c26caced1affd187e46ff614b73d9e5e5b2029
kcbw <kcbw_tn@tutanota.com>
2019-11-03 12:44:16 -0800

    Moved weapon properties into static dictionary

--- scenes/weapon/Sniper.gd
+++ scenes/weapon/Sniper.gd
@@ -1,15 +1,28 @@
 extends Weapon
 
+static func get_weapon_props():
+	return {
+		'clip_size': 5,
+		'weapon_name': 'Sniper',
+		'price': 40,
+		'reload_time': 3,
+		'fire_rate': 1,
+		'spread': PI/128,
+		'num_projectiles': 1,
+		'automatic': false
+	}
+
 func _ready():
-	clip_size   = 5
-	clip        = 5
-	weapon_name = 'Pistol'
-	price       = 100
-	reload_time = 3
-	fire_rate = 1
-	spread = PI/128
-	num_projectiles = 1
-	automatic = false
+	var weapon_props = get_weapon_props()
+	clip_size   	= weapon_props['clip_size']
+	clip        	= weapon_props['clip_size']
+	weapon_name 	= weapon_props['weapon_name']
+	price       	= weapon_props['price']
+	reload_time 	= weapon_props['reload_time']
+	fire_rate   	= weapon_props['fire_rate']
+	spread      	= weapon_props['spread']
+	num_projectiles = weapon_props['num_projectiles']
+	automatic 		= weapon_props['automatic']
 	
 func _gen_projectile():
 	var projectile = Projectile.instance()
