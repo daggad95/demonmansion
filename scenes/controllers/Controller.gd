c9c6cb030875e7a938a713c1bd3e426582e8110d
David Garber <daggad95@gmail.com>
2020-03-21 20:27:41 -0700

    Begin refactoring controller

--- scenes/controllers/Controller.gd
+++ scenes/controllers/Controller.gd
@@ -12,59 +12,54 @@ signal player_shoot
 signal player_not_shoot
 signal player_open_store
 
-const min_joy_value = 0.05
-const menu_threshold = 0.8
+const threshold = 0.5
+const menu_delay = 0.25
 var device
-var mapping = {}
+var signals = [
+	 {
+		"name": "menu_up",
+		"emit_value": false,
+		"triggers": [
+			{
+				"is_analog": true,
+				"mapping": JOY_AXIS_1,
+				"dir": 1,
+			}
+		],
+		"timer": Timer.new(),
+		"delay": menu_delay
+	}
+]
 
 func init(device):
-	self.device = device
-	self.mapping['menu_select'] = JOY_XBOX_A
-	self.mapping['menu_back'] = JOY_XBOX_B
-	self.mapping['menu_select_secondary'] = JOY_XBOX_Y
-	self.mapping['player_open_store'] = JOY_XBOX_Y
+	for signal_data in signals:
+		signal_data['timer'].one_shot = true
 
-func _physics_process(delta):
-	var analog_values = {
-		'left_joy_x': Input.get_joy_axis(device, JOY_AXIS_0),
-		'left_joy_y': Input.get_joy_axis(device, JOY_AXIS_1),
-		'right_joy_x': Input.get_joy_axis(device, JOY_AXIS_2),
-		'right_joy_y': Input.get_joy_axis(device, JOY_AXIS_3),
-		'right_trigger': Input.get_joy_axis(device, JOY_AXIS_7)
-	}
+func _handle_signal(signal_data):
+	var triggers = signal_data['triggers']
+	var signal_name = signal_data['name']
+	var timer = signal_data['timer']
+	var delay = signal_data['delay']
 	
-	for key in analog_values.keys():
-		if abs(analog_values[key]) < min_joy_value:
-			analog_values[key] = 0
-	
-	if abs(analog_values['left_joy_x']) > menu_threshold:
-		if analog_values['left_joy_x'] > 0:
-			emit_signal("menu_right")
-		if analog_values['left_joy_x'] < 0:
-			emit_signal("menu_left")
-	
-	if analog_values['left_joy_y'] > menu_threshold:
-		if analog_values['left_joy_y']  > 0:
-			emit_signal("menu_down")
-		if analog_values['left_joy_y'] < 0:
-			emit_signal("menu_up")
+	for trigger in triggers:
+		if timer.is_stopped():
+			if trigger["is_analog"]:
+					var value = Input.get_joy_axis(device, trigger["mapping"])
 		
-	emit_signal("player_move", Vector2(
-		analog_values['left_joy_x'], 
-		analog_values['left_joy_y']
-	).normalized())
-	
-	if abs(analog_values['right_joy_x']) > 0 or abs(analog_values['right_joy_y']) > 0:
-		emit_signal("player_aim", Vector2(
-			analog_values['right_joy_x'],
-			analog_values['right_joy_y']
-		).normalized())
-	
-	if abs(Input.get_joy_axis(device, JOY_AXIS_7)) > min_joy_value:
-		emit_signal("player_shoot")
-	else:
-		emit_signal("player_not_shoot")
-	
-	for signal_name in mapping.keys():
-		if Input.is_joy_button_pressed(device, mapping[signal_name]):
-			emit_signal(signal_name)
+					if (trigger["dir"] > 0 and value > threshold or
+						trigger["dir"] < 0 and value < -threshold):
+						
+						if trigger['emit_value']:
+							emit_signal(signal_name, value)
+						else:
+							emit_signal(signal_name)
+						
+						if delay > 0:
+							timer.start(delay)
+			else:
+				if Input.is_joy_button_pressed(device, trigger['mapping']):
+					emit_signal(signal_name)
+				
+func _physics_process(delta):
+	for signal_data in signals:
+		_handle_signal(signal_data)
