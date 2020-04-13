f960d5a32975d593db4c95d6ded5ddc6e00739ab
kcbw <kcbw_tn@tutanota.com>
2020-04-12 18:28:33 -0700

    Add scrolling player sprites to selection screen

--- scenes/menu/character_selection/SelectionScreen/SelectionScene.gd
+++ scenes/menu/character_selection/SelectionScreen/SelectionScene.gd
@@ -11,11 +11,33 @@ func _on_BackButton_pressed():
 
 func _ready():
 	var controllers = get_node("/root/Controllers").get_controllers()
+	var sprite_icons = get_icons()
+	
 	for idx in range(4):
 		var new_userbox = Userbox.instance()
-		new_userbox.init()
+		new_userbox.init(sprite_icons)
 
 		if(idx < len(controllers)):
 			new_userbox.link_controller(controllers[idx])
 		
 		hbox_userboxes.add_child(new_userbox)
+
+func get_icons():
+	var icons = []
+	var path = "res://assets/sprites/player/"
+	var dir = Directory.new()
+	dir.open(path)
+	dir.list_dir_begin()
+
+	while true:
+		var file_name = dir.get_next()
+		if file_name == "":
+			# break the while loop when get_next() returns ""
+			break
+		elif !file_name.begins_with(".") && !file_name.ends_with(".import"):
+			var tex_subregion = AtlasTexture.new()
+			tex_subregion.set_atlas(load(path + file_name))
+			tex_subregion.set_region(Rect2(0,0,64,128))
+			icons.append(tex_subregion)
+			
+	return icons
--- scenes/menu/character_selection/userbox/Userbox.gd
+++ scenes/menu/character_selection/userbox/Userbox.gd
@@ -6,14 +6,18 @@ const State = preload("res://scripts/State.gd")
 onready var NameSelector = $VBoxContainer/PlayerNameContainer/PlayerName
 onready var UpArrow = $VBoxContainer/FrameContainer/VBoxContainer/UpArrow
 onready var DownArrow = $VBoxContainer/FrameContainer/VBoxContainer/DownArrow
+onready var PlayerIcon = $VBoxContainer/FrameContainer/VBoxContainer/MarginContainer/MarginContainer/PlayerIcon
 
 var controller = null
 var current_state = null
+var current_sprite_idx = 0
 var fsm
+var sprite_icons
 
-func init():
+func init(sprite_icons):
 	fsm = StateMachine.new()
 	fsm.init(self, Empty.new())
+	self.sprite_icons = sprite_icons
 
 func link_controller(controller):
 	self.controller = controller
@@ -38,6 +42,22 @@ func hide_player_arrows():
 	UpArrow.set_modulate(Color(1,1,1,0))
 	DownArrow.set_modulate(Color(1,1,1,0))
 
+func reset_icon():
+	current_sprite_idx = 0
+	PlayerIcon.set_texture(sprite_icons[current_sprite_idx])
+
+func increment_icon():
+	current_sprite_idx += 1
+	if current_sprite_idx == 12:
+		current_sprite_idx = 0
+	PlayerIcon.set_texture(sprite_icons[current_sprite_idx])
+	
+func decrement_icon():
+	current_sprite_idx += -1
+	if current_sprite_idx == -1:
+		current_sprite_idx = 11
+	PlayerIcon.set_texture(sprite_icons[current_sprite_idx])
+
 class Empty extends State:
 	func enter(model):
 		state_name = "Empty"
@@ -53,6 +73,7 @@ class Empty extends State:
 	func exit():
 		model.show()
 		model.hide_player_arrows()
+		model.reset_icon()
 		
 class NameFocused extends State:
 	func enter(model):
@@ -70,14 +91,13 @@ class NameFocused extends State:
 				return Empty.new()
 			else:
 				model.NameSelector.reset()
+				model.reset_icon()
 				return NameFocused.new()
-			
 		elif(input_name == "menu_up"):
 			model.NameSelector.decrement()
 		elif(input_name == "menu_down"):
 			model.NameSelector.increment()
 		return null
-
 
 class PlayerFocused extends State:
 	func enter(model):
@@ -92,8 +112,11 @@ class PlayerFocused extends State:
 		elif(input_name == "menu_back"):
 			model.hide_player_arrows()
 			return NameFocused.new()
+		elif(input_name == "menu_up"):
+			model.increment_icon()
+		elif(input_name == "menu_down"):
+			model.decrement_icon()
 			
-		
 class Ready extends State:
 	func enter(model):
 		state_name = "Ready"
